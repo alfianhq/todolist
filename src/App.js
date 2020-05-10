847652a7138d8d99e49bd5e1eecf56b5d7288bca
alfianhq <63658875+alfianhq@users.noreply.github.com>
2020-05-11 06:16:28 +0700

    uuid setting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, {Component} from 'react';
-import uuid from 'uuid';
+import uuid from 'react-uuid';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import TodoInput from './component/TodoInput';
 import TodoList from './component/TodoList';
 
 export default class App extends Component {
+	state = {
+		items: [
+			{id: 1, title: 'wake up'},
+			{id: 2, title: 'break fast'},
+		],
+		id: uuid(),
+	};
+
 	render() {
+		console.log(this.state);
 		return (
 			<div>
 				<div className='container'>
