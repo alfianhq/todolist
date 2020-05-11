89b11da1864d116c2ffe21fa6b8144192ee08bcc
alfianhq <63658875+alfianhq@users.noreply.github.com>
2020-05-11 10:08:53 +0700

    handle Input

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ export default class App extends Component {
 				<div className='container'>
 					<div className='row'>
 						<div className='col-10 mx-auto col-md-8 mt-5'>
-							<h3 className='text-capitalize text-center'></h3>
+							<h3 className='text-capitalize text-center'>List Item</h3>
 							<TodoInput
 								item={this.state.item}
 								handleChange={this.handleChange}
